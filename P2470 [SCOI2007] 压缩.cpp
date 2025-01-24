914cd8f427ea91736d8d6bb10a2eb4609a9a32a6
自己家 <bobi20110114@outlook.com>
2025-01-24 17:27:03 +0800

    ac P2470 [SCOI2007] 压缩

---
+++ P2470 [SCOI2007] 压缩.cpp
@@ -0,0 +1,164 @@
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+//
+//      By txp2024 www.luogu.com.cn  TXP2023 www.github.com
+// 
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+#pragma once
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+#include <string.h>
+#include <ctype.h>
+#include <string>
+#include <cstdarg>
+#include <climits>
+#include <time.h>
+#include <iostream>
+#include <stdint.h>
+#include <time.h>
+
+#define READ false
+#define MAX_INF 1e18
+#define MAX_NUM_SIZE 35
+
+typedef long long int ll;
+typedef unsigned long long int unill;
+
+//快读函数声明
+#if READ
+template< typename T >
+inline T readf();
+#else
+template< typename Type >
+inline Type readf(Type* p = NULL);
+#endif
+
+//快速输出函数
+template<typename Type>
+inline void writef(Type x);
+
+std::vector< std::vector< std::vector< ll> > > dp;
+std::string str;
+ll length;
+
+int main() {
+#ifdef _FREOPEN
+    freopen("input.txt", "r", stdin);
+#endif // _FREOPEN
+
+#ifdef _RUN_TIME
+    clock_t start = clock();
+#endif // _RUN_TIME
+
+    //TODO
+    std::cin >> str;
+    
+    length = str.length();
+    dp.resize(length, 
+        std::vector< std::vector<ll> >(
+            length, 
+            std::vector<ll>(
+                2, 
+                1
+            ))
+    );
+    for (size_t l = 0; l < length; l++) {
+        for (size_t r = l; r < length; r++) {
+            dp[l][r][0] = r - l + 1;
+            dp[l][r][1] = r - l + 1;
+        }
+    }
+    for (size_t len = 1; len < length; len++) {
+        for (size_t l = 0; l + len < length; l++) {
+            ll r = l + len, mid = (l + r) >> 1;
+            dp[l][r][0] = r - l + 1;
+            dp[l][r][1] = r - l + 1;
+            if (str.substr(l, mid - l + 1) == str.substr(mid + 1, r - mid)) {
+                dp[l][r][0] = std::min(dp[l][r][0], dp[l][mid][0] + 1);
+            }
+            for (size_t k = l; k < r; k++) {
+                dp[l][r][0] = std::min(dp[l][r][0], ll(dp[l][k][0] + r - k));
+            }
+            for (size_t k = l; k < r; k++)
+                dp[l][r][1] = std::min(
+                    dp[l][r][1], 
+                    std::min(dp[l][k][0], dp[l][k][1]) + std::min(dp[k + 1][r][0], dp[k + 1][r][1]) + 1
+                );
+        }
+    }
+    printf("%lld\n", std::min(dp[0][length - 1][1], dp[0][length - 1][0]));
+
+
+
+#ifdef _RUN_TIME
+    printf("The running duration is not less than %ld ms\n", clock() - start);
+#endif // _RUN_TIME
+    return 0;
+}
+
+#if READ
+template< typename T >
+inline T readf() {
+#if false
+    T sum = 0;
+    char ch = getchar();
+    while (ch > '9' || ch < '0') ch = getchar();
+    while (ch >= '0' && ch <= '9') sum = sum * 10 + ch - 48, ch = getchar();
+    return sum;
+#else
+    T ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    return sgn ? -ret : ret;
+#endif
+}
+#else
+template< typename Type >
+inline Type readf(Type* p) {
+    Type ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    if (p != NULL) {
+        *p = Type(sgn ? -ret : ret);
+    }
+    return sgn ? -ret : ret;
+}
+#endif
+
+template<typename Type>
+inline void writef(Type x) {
+    int sta[MAX_NUM_SIZE];
+    int top = 0;
+    do {
+        sta[top++] = x % 10, x /= 10;
+    } while (x);
+    while (top) putchar(sta[--top] + '0');  // 48 是 '0'
+    return;
+}
+
+
+
+/**
+ *              ,----------------,              ,---------,
+ *         ,-----------------------,          ,"        ,"|
+ *       ,"                      ,"|        ,"        ,"  |
+ *      +-----------------------+  |      ,"        ,"    |
+ *      |  .-----------------.  |  |     +---------+      |
+ *      |  |                 |  |  |     | -==----'|      |
+ *      |  |  希望此代码无bug|  |  |     |         |      |
+ *      |  |                 |  |  |     |`---=    |      |
+ *      |  |  C:\>_          |  |  |     |==== ooo |      ;
+ *      |  |                 |  |  |     |(((( [33]|    ,"
+ *      |  `-----------------'  | /      |((((     |  ,"
+ *      +-----------------------+/       |         |,"
+ *         /_)______________(_/          +---------+
+ *    _______________________________
+ *   /  oooooooooooooooo  .o.  oooo /,   /-----------
+ *  / ==ooooooooooooooo==.o.  ooo= //   /\--{)B     ,"
+ * /_==__==========__==_ooo__ooo=_/'   /___________,"
+ *
+ */
