fba80c745be0d0a769ef7f88559033a8440de649
二附中 <bobi20110114@outlook.com>
2025-03-05 17:25:58 +0800

    P3369

--- P3369 【模板】普通平衡树    Splay方法.cpp
+++ P3369 【模板】普通平衡树    Splay方法.cpp
@@ -36,52 +36,199 @@ inline Type readf(Type* p = nullptr);
 template<typename Type>
 inline void writef(Type x);
 
-namespace Splay {
-    struct Splay_data {
-        short int child[2]; //
-        ll father; //父亲节点
-        ll value; //节点权值
-        ll size; //子树大小
-        ll cnt;//此节点所表示的权值的出现次数
 
-        Splay_data() {
-            child[0] = 0, child[1] = 0;
-            father = 0;
-            value = 0;
-            size = 0;
-            cnt = 0;
-            return;
-        }
-    };
+struct Splay_data {
+    short int child[2]; //
+    ll father; //父亲节点
+    ll value; //节点权值
+    ll size; //子树大小
+    ll cnt;//此节点所表示的权值的出现次数
 
-    inline void update_size(ll _Index); //在改变节点位置后，将节点 _P 的 size 更新。
+    Splay_data() {
+        child[0] = 0, child[1] = 0;
+        father = 0;
+        value = 0;
+        size = 0;
+        cnt = 0;
+        return;
+    }
+};
 
-    inline bool get_child(ll _Index); //判断节点 x 是父亲节点的左儿子还是右儿子。 左false 右true
 
-    inline void remove(ll _Index);
-
-    inline void rotate(ll _Index); //将点_Index和其父节点旋转
-}
-
-Splay::Splay_data tree[MAXN];
+Splay_data tree[MAXN];
 ll root, node_num;//根节点编号  节点个数
 
-void Splay::update_size(ll _Index) {
+//在改变节点位置后，将节点 _P 的 size 更新。
+void update_size(ll _Index) {
     tree[_Index].size = tree[tree[_Index].child[0]].size + tree[tree[_Index].child[1]].size + tree[_Index].cnt;
     return;
 }
 
-bool Splay::get_child(ll _Index) {
+//判断节点 x 是父亲节点的左儿子还是右儿子。 左false 右true
+bool get_child(ll _Index) {
     return _Index == tree[tree[_Index].father].child[1];
 }
 
-void Splay::remove(ll _Index) {
-    tree[_Index] = Splay::Splay_data();
+//销毁节点_Index
+void clear(ll _Index) {
+    tree[_Index] = Splay_data();
     return;
 }
 
-void Splay::rotate(ll _Index) {
+//将点_Index和其父节点旋转
+void rotate(ll _Index) {
+    //旋转需要保证:
+    //  整棵 Splay 的中序遍历不变（不能破坏二叉查找树的性质）。
+    //  受影响的节点维护的信息依然正确有效。
+    //  root 必须指向旋转后的根节点。
+    // 
+    // 具体步骤
+    // 1.将 y 的左儿子指向 x 的右儿子，且 x 的右儿子（如果 x 有右儿子的话）的父亲指向 y
+    // 2.将 x 的右儿子指向 y，且 y 的父亲指向 x
+    // 3.如果原来的 y 还有父亲 z，那么把 z 的某个儿子（原来 y 所在的儿子位置）指向 x，且 x 的父亲指向 z。
+    //int y = fa[x], z = fa[y], chk = get(x);
+    //ch[y][chk] = ch[x][chk ^ 1];
+    //if (ch[x][chk ^ 1]) fa[ch[x][chk ^ 1]] = y;
+    //ch[x][chk ^ 1] = y;
+    //fa[y] = x;
+    //fa[x] = z;
+    //if (z) ch[z][y == ch[z][1]] = x;
+    //maintain(y);
+    //maintain(x);
+    ll u = tree[_Index].father, v = tree[u].father, chk = get_child(_Index);
+    //将 y 的左儿子指向 x 的右儿子
+    tree[u].child[chk] = tree[u].child[chk ^ 1];
+    //x 的右儿子（如果 x 有右儿子的话）的父亲指向 y
+    if (tree[_Index].child[chk ^ 1]) {
+        tree[tree[_Index].child[chk ^ 1]].father = u;
+    }
+    tree[_Index].child[chk ^ 1] = u;
+    tree[u].father = _Index;
+    //如果原来的 y 还有父亲 z，那么把 z 的某个儿子（原来 y 所在的儿子位置）指向 x，且 x 的父亲指向 z。
+    tree[_Index].father = v;
+    if (v){
+        tree[v].child[get_child(u)] = _Index;
+    }
+    update_size(_Index);
+    update_size(u);
+    return;
+}
 
+//Splay 操作
+void Splay(ll _Index) {
+    //Splay 操作规定：每访问一个节点 x 后都要强制将其旋转到根节点。
+    //
+    for (ll i = tree[_Index].father; i = tree[_Index].father, i ; rotate(i)) {
+        if (tree[i].father) {
+            rotate(get_child(_Index) == get_child(i) ? i : _Index);
+        }
+    }
+    root = _Index;
+    return;
+}
+
+//插入值 _Value
+inline void insert(ll _Value) {
+    /*
+    插入操作是一个比较复杂的过程，具体步骤如下（假设插入的值为 k）：
+    如果树空了，则直接插入根并退出。
+    如果当前节点的权值等于 k 则增加当前节点的大小并更新节点和父亲的信息，将当前节点进行 Splay 操作。
+    /否则按照二叉查找树的性质向下找，找到空节点就插入即可（请不要忘记 Splay 操作）。
+    */
+
+    //代码实现如下
+    //如果树空了，则直接插入根并退出。
+    if (!root) {
+        tree[++node_num].father = 0;
+        tree[node_num].cnt++;
+        tree[node_num].value = _Value;
+        root = node_num;
+        update_size(node_num);
+        return;
+    }
+    ll _Index = root, father = 0;
+    while (1) {
+        if (tree[_Index].value == _Value) {
+            tree[_Index].cnt++;
+            update_size(_Index);
+            update_size(father);
+            Splay(_Index);
+            break;
+        }
+        father = _Index;
+        _Index = tree[_Index].child[tree[_Index].value < _Value/*如果当前节点值小于插入节点值 表达式值为1 对应的右子节点 反之*/];
+        //没有点的值为_Value，那么就要插入节点
+        if (!_Index) {
+            tree[++node_num].value = _Value;
+            tree[node_num].cnt++;
+            tree[node_num].father = father;
+            tree[father].child[_Value > tree[father].value] = node_num;
+            update_size(node_num);
+            update_size(father);
+            Splay(node_num);
+            break;
+        }
+    }
+    return;
+}
+
+//查询 _Value 的排名
+inline ll get_Rank_By_Value(ll _Value) {
+    ll _Index = root, ret = 0;
+    while (1) {
+        if (_Value < tree[_Index].value) {
+            _Index = tree[_Index].child[0];
+        }
+        else {
+            ret += tree[tree[_Index].child[0]].size;
+            if (!_Index) {
+                return ret + 1;
+            }
+            if (tree[_Index].value == _Value) {
+                Splay(_Index);
+                return ret + 1;
+            }
+            ret += tree[_Index].cnt;
+            _Index = tree[_Index].child[1];
+        }
+    }
+}
+
+//查询排名 rank 的数
+inline ll get_Value_By_Rank(ll _Rank) {
+    ll _Index = root;
+    while (1) {
+        if (tree[_Index].child[0] && tree[_Index].size > _Rank) {
+            _Index = tree[_Index].child[0];
+        }
+        else {
+            _Rank -= tree[_Index].size + tree[_Index].cnt;
+            if (_Rank <= 0) {
+                Splay(_Index);
+                return tree[_Index].value;
+            }
+            _Index = tree[_Index].child[1];
+        }
+    }
+    return;
+}
+
+inline void remove(ll _Value) {
+    //首先将 x 旋转到根的位置。
+    get_Rank_By_Value(_Value);
+
+}
+
+inline ll precursor(ll _Value) {
+    insert(_Value);
+    ll _Index = tree[root].child[0];
+    if (_Index) {
+        while (tree[_Index].child[1]) {
+            _Index = tree[_Index].child[1];
+        }
+    }
+    ll ret = tree[_Index].value;
+    
 }
 
 int main() {
