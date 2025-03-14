6f809abf6876a53d9b34fbedf8b3aa0565f015a7
二附 <bobi20110114@outlook.com>
2025-03-14 20:27:12 +0800

    ac P2015

---
+++ P2015 二叉苹果树.cpp
@@ -0,0 +1,172 @@
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+//
+//      By txp2024 www.luogu.com.cn  TXP2023 www.github.com
+// 
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+#pragma once
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+#include <ctype.h>
+#include <cstdarg>
+#include <climits>
+#include <time.h>
+#include <iostream>
+#include <stdint.h>
+#include <time.h>
+
+#define READ          false
+#define MAX_INF       1e18
+#define MAX_NUM_SIZE  35
+#define MAXN          (int64_t)(1e2 + 5)
+
+typedef long long int ll;
+typedef unsigned long long int unill;
+
+//快读函数声明
+#if READ
+template< typename T >
+inline T readf();
+#else
+template< typename Type >
+inline Type readf(Type* p = nullptr);
+#endif
+
+//快速输出函数
+template<typename Type>
+inline void writef(Type x);
+
+struct Edge {
+    ll v, w;
+    Edge(ll _v, ll _w) {
+        v = _v;
+        w = _w;
+        return;
+    }
+};
+
+std::vector<Edge> graph[MAXN];
+ll dpTable[MAXN][MAXN], son[MAXN][2];
+ll n, q;
+
+inline void init(ll _u, ll _father, ll _value) {
+    dpTable[_u][0] = 0;
+    dpTable[_u][1] = _value;
+    ll cnt = 0;
+    for (Edge i : graph[_u]) {
+        if (i.v == _father) {
+            continue;
+        }
+        son[_u][cnt++] = i.v;
+        init(i.v, _u, i.w);
+    }
+}
+
+inline ll fill_dpTable(ll _u) {
+    if (son[_u][0]) {
+        fill_dpTable(son[_u][0]);
+    }
+    if (son[_u][1]) {
+        fill_dpTable(son[_u][1]);
+    }
+    for (size_t i = 2; i <= q + 1; i++) {
+        for (size_t k = 0; k < i; k++) {
+            dpTable[_u][i] = std::max(dpTable[_u][i], dpTable[_u][1] + dpTable[son[_u][0]][k] + dpTable[son[_u][1]][i - 1 - k]);
+        }
+    }
+    return dpTable[1][q + 1];
+}
+
+int main() {
+#ifdef _FREOPEN
+    freopen("input.txt", "r", stdin);
+#endif // _FREOPEN
+
+#ifdef _RUN_TIME
+    clock_t start = clock();
+#endif // _RUN_TIME
+
+    //TODO
+    readf(&n), readf(&q);
+
+    for (size_t i = 0; i < n - 1; i++) {
+        ll u = readf<ll>(), v = readf<ll>(), w = readf<ll>();
+        graph[u].push_back(Edge(v, w));
+        graph[v].push_back(Edge(u, w));
+    }
+
+    init(1, -1, 0);
+    printf("%lld\n", fill_dpTable(1));
+
+#ifdef _RUN_TIME
+    printf("The running duration is not less than %ld ms\n", clock() - start);
+#endif // _RUN_TIME
+    return 0;
+}
+
+#if READ
+template< typename T >
+inline T readf() {
+#if false
+    T sum = 0;
+    char ch = getchar();
+    while (ch > '9' || ch < '0') ch = getchar();
+    while (ch >= '0' && ch <= '9') sum = sum * 10 + ch - 48, ch = getchar();
+    return sum;
+#else
+    T ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    return sgn ? -ret : ret;
+#endif
+}
+#else
+template< typename Type >
+inline Type readf(Type* p) {
+    Type ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    if (p != NULL) {
+        *p = Type(sgn ? -ret : ret);
+    }
+    return sgn ? -ret : ret;
+}
+#endif
+
+template<typename Type>
+inline void writef(Type x) {
+    int sta[MAX_NUM_SIZE];
+    int top = 0;
+    do {
+        sta[top++] = x % 10, x /= 10;
+    } while (x);
+    while (top) putchar(sta[--top] + '0');  // 48 是 '0'
+    return;
+}
+
+
+
+/**
+ *              ,----------------,              ,---------,
+ *         ,-----------------------,          ,"        ,"|
+ *       ,"                      ,"|        ,"        ,"  |
+ *      +-----------------------+  |      ,"        ,"    |
+ *      |  .-----------------.  |  |     +---------+      |
+ *      |  |                 |  |  |     | -==----'|      |
+ *      |  |  希望此代码无bug|  |  |     |         |      |
+ *      |  |                 |  |  |     |`---=    |      |
+ *      |  |  C:\>_          |  |  |     |==== ooo |      ;
+ *      |  |                 |  |  |     |(((( [33]|    ,"
+ *      |  `-----------------'  | /      |((((     |  ,"
+ *      +-----------------------+/       |         |,"
+ *         /_)______________(_/          +---------+
+ *    _______________________________
+ *   /  oooooooooooooooo  .o.  oooo /,   /-----------
+ *  / ==ooooooooooooooo==.o.  ooo= //   /\--{)B     ,"
+ * /_==__==========__==_ooo__ooo=_/'   /___________,"
+ *
+ */
