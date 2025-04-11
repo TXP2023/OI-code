597bee3e78725ae79b6e406c6de590c8564f4b3d
新沛谭 <bobi20110114@outlook.com>
2025-04-11 20:28:56 +0800

    6739

--- P6739 [BalticOI 2014] Three Friends (Day1).cpp
+++ P6739 [BalticOI 2014] Three Friends (Day1).cpp
@@ -20,6 +20,10 @@
 #define MAX_NUM_SIZE  35
 #define MAX_LENGTH    (uint64_t)(2e6+1)
 #define _DEBUG        true
+#define HASH_1_BASE   998244353
+#define HASH_1_MOD    22783
+#define HASH_2_BASE   37951
+#define HASH_2_MOD    1000000007
 
 typedef long long int ll;
 typedef unsigned long long int unill;
@@ -37,62 +41,61 @@ inline Type readf(Type* p = nullptr);
 template<typename Type>
 inline void writef(Type x);
 
-template<size_t _MaxStrLenght>
+template<size_t _MaxStrLenght, uint64_t _base, uint64_t _mod>
 class Hash {
 public:
     Hash();
     Hash(std::string _Str);
 
     inline uint64_t get_range_hash(size_t _First_Pos, size_t _Last_Pos);
+
+    inline uint64_t fast_pow(size_t bas, size_t pow);
+
+    uint64_t base = _base;
 private:
     uint64_t hash[_MaxStrLenght];
     
 };
 
-#if _DEBUG
-uint64_t base = 2, Mod = UINT64_MAX;
-#else
-uint64_t base = 12357166273, Mod = 1e9 + 7;
-#endif // _DEBUG
-
-
 std::string str;
-Hash<MAX_LENGTH> hash;
+Hash<MAX_LENGTH, HASH_1_BASE, HASH_1_MOD> hash_1;
+Hash<MAX_LENGTH, HASH_2_BASE, HASH_2_MOD> hash_2;
 size_t n, ans_l, ans_r;
 
-inline uint64_t fast_pow(size_t bas, size_t pow) {
+template<size_t _MaxStrLenght, uint64_t _base, uint64_t _mod>
+inline uint64_t Hash<_MaxStrLenght, _base, _mod>::fast_pow(size_t bas, size_t pow) {
     uint64_t ret = 1;
     while (pow > 0) {
         if (pow & 1) {
             ret *= bas;
         }
         bas *= bas;
-        bas %= Mod;
+        bas %= _mod;
         pow >>= 1;
     }
-    return ret % Mod;
+    return ret % _mod;
 }
 
-template<size_t _MaxStrLenght>
-Hash<_MaxStrLenght>::Hash() {
+template<size_t _MaxStrLenght, uint64_t _base, uint64_t _mod>
+Hash<_MaxStrLenght, _base, _mod>::Hash() {
     memset(hash, 0, sizeof(hash));
     return;
 }
 
-template<size_t _MaxStrLenght>
-Hash<_MaxStrLenght>::Hash(std::string _Str) {
+template<size_t _MaxStrLenght, uint64_t _base, uint64_t _mod>
+Hash<_MaxStrLenght, _base, _mod>::Hash(std::string _Str) {
     for (size_t i = 1; i <= _Str.length(); i++) {
-        hash[i] = (hash[i - 1] * base + _Str[i - 1] - 'A' + 1) % Mod;
+        hash[i] = (hash[i - 1] * base + _Str[i - 1] - 'A' + 1) % _mod;
     }
     return;
 }
 
-template<size_t _MaxStrLenght>
-inline uint64_t Hash<_MaxStrLenght>::get_range_hash(size_t _First_Pos, size_t _Last_Pos) {
+template<size_t _MaxStrLenght, uint64_t _base, uint64_t _mod>
+inline uint64_t Hash<_MaxStrLenght, _base, _mod>::get_range_hash(size_t _First_Pos, size_t _Last_Pos) {
     if (_First_Pos > _Last_Pos) {
         return 0;
     }
-    return hash[_Last_Pos] - hash[_First_Pos - 1] * fast_pow(base, _Last_Pos - _First_Pos + 1) % Mod;
+    return hash[_Last_Pos] - hash[_First_Pos - 1] * fast_pow(base, _Last_Pos - _First_Pos + 1) % _mod;
 }
 
 int main() {
@@ -112,20 +115,25 @@ int main() {
     }
     std::cin >> str;
 
-    hash = Hash<MAX_LENGTH>(str);
+    hash_1 = Hash<MAX_LENGTH, HASH_1_BASE, HASH_1_MOD>(str);
+    hash_2 = Hash<MAX_LENGTH, HASH_2_BASE, HASH_2_MOD>(str);
 
     ll size = n / 2;
     for (size_t i = 1; i <= n; i++) {
-        uint64_t hash_1, hash_2;
+        uint64_t hash_1_1, hash_1_2, hash_2_1, hash_2_2;
         if (i <= n/2) {
-            hash_1 = hash.get_range_hash(1, i - 1) * fast_pow(base, n/2 - i + 1) + hash.get_range_hash(i + 1, size + 1);
-            hash_2 = hash.get_range_hash(size + 2, n);  
-            if (hash_1 == hash_2) {
+            hash_1_1 = hash_1.get_range_hash(1, i - 1) * hash_1.fast_pow(hash_1.base, n/2 - i + 1) + hash_1.get_range_hash(i + 1, size + 1);
+            hash_1_2 = hash_1.get_range_hash(size + 2, n);
+            hash_2_1 = hash_2.get_range_hash(1, i - 1) * hash_2.fast_pow(hash_2.base, n / 2 - i + 1) + hash_2.get_range_hash(i + 1, size + 1);
+            hash_2_2 = hash_2.get_range_hash(size + 2, n);
+            if (hash_1_1 == hash_1_2 && hash_2_1 == hash_2_2) {
                 if (ans_l == 0) {
                     ans_l = size + 2;
                 }
                 else {
-                    if (hash.get_range_hash(ans_l, ans_l + size - 1) != hash.get_range_hash(size + 2, size + 2 + size - 1)) {
+                    if (hash_1.get_range_hash(ans_l, ans_l + size - 1) != hash_1.get_range_hash(size + 2, size + 2 + size - 1) &&
+                        hash_2.get_range_hash(ans_l, ans_l + size - 1) != hash_2.get_range_hash(size + 2, size + 2 + size - 1)
+                        ) {
                         puts("NOT UNIQUE");
                         exit(0);
                     }
@@ -133,14 +141,18 @@ int main() {
             }
         }
         else {
-            hash_1 = hash.get_range_hash(1, n/2);
-            hash_2 = hash.get_range_hash(n/2 + 1, i - 1) * fast_pow(base, n - i) + hash.get_range_hash(i + 1, n);
-            if (hash_1 == hash_2) {
+            hash_1_1 = hash_1.get_range_hash(1, n/2);
+            hash_1_2 = hash_1.get_range_hash(n/2 + 1, i - 1) * hash_1.fast_pow(hash_1.base, n - i) + hash_1.get_range_hash(i + 1, n);
+            hash_2_1 = hash_2.get_range_hash(1, n / 2);
+            hash_2_2 = hash_2.get_range_hash(n / 2 + 1, i - 1) * hash_2.fast_pow(hash_2.base, n - i) + hash_2.get_range_hash(i + 1, n);
+            if (hash_1_1 == hash_1_2 && hash_2_1 == hash_2_2) {
                 if (ans_l == 0) {
                     ans_l = 1;
                 }
                 else {
-                    if (hash.get_range_hash(ans_l, ans_l + size - 1) != hash.get_range_hash(1, size)) {
+                    if (hash_1.get_range_hash(ans_l, ans_l + size - 1) != hash_1.get_range_hash(1, size) &&
+                        hash_2.get_range_hash(ans_l, ans_l + size - 1) != hash_2.get_range_hash(1, size)
+                        ) {
                         puts("NOT UNIQUE");
                         exit(0);
                     }
