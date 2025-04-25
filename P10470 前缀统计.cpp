7266c5501f4e600ac12b8f577478f32dd1c252dc
Student <Student@38>
2025-04-25 19:39:48 +0800

    ac 10470

---
+++ P10470 前缀统计.cpp
@@ -0,0 +1,170 @@
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+//
+//      By txp2024 www.luogu.com.cn  TXP2023 www.github.com
+// 
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+#pragma once
+#include <vector>
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <ctype.h>
+#include <cstdarg>
+#include <climits>
+#include <time.h>
+#include <iostream>
+#include <stdint.h>
+
+#define READ          false
+#define MAX_INF       1e18
+#define MAX_NUM_SIZE  35
+#define TRIE_LENGTH   (size_t)(1e6 + 5)
+#define ROOT_INDEX    0
+
+typedef long long int ll;
+typedef unsigned long long int unill;
+
+//快读函数声明
+#if READ
+template< typename T >
+inline T readf();
+#else
+template< typename Type >
+inline Type readf(Type* p = nullptr);
+#endif
+
+//快速输出函数
+template<typename Type>
+inline void writef(Type x);
+
+struct Trie_Node {
+    size_t Index[26];
+    size_t cnt;
+
+    Trie_Node() {
+        memset(Index, 0, sizeof(Index));
+        cnt = 0;
+        return;
+    }
+};
+
+Trie_Node trie[TRIE_LENGTH];
+ll n, m, trie_cnt = 0;
+
+void insert(std::string _Str, size_t _Cnt, size_t _Pos) {
+    if (_Cnt == _Str.length()) {
+        ++trie[_Pos].cnt;
+        return;
+    }
+
+    if (!trie[_Pos].Index[_Str[_Cnt] - 'a']) {
+        trie[_Pos].Index[_Str[_Cnt] - 'a'] = ++trie_cnt;
+    }
+    insert(_Str, _Cnt + 1, trie[_Pos].Index[_Str[_Cnt] - 'a']);
+    return;
+}
+
+uint64_t query(std::string _Str, size_t _Cnt, const size_t & _Pos) {
+    if (_Cnt == _Str.length() || !trie[_Pos].Index[_Str[_Cnt] - 'a']) {
+        return trie[_Pos].cnt;
+    }
+    return query(_Str, ++_Cnt, trie[_Pos].Index[_Str[_Cnt] - 'a']) + trie[_Pos].cnt;
+}
+
+int main() {
+#ifdef _FREOPEN
+    freopen("input.txt", "r", stdin);
+#endif // _FREOPEN
+
+#ifdef _RUN_TIME
+    clock_t start = clock();
+#endif // _RUN_TIME
+
+    //TODO
+    readf(&n), readf(&m);
+
+    for (size_t i = 0; i < n; i++) {
+        std::string str;
+        std::cin >> str;
+        insert(str, 0, ROOT_INDEX);
+    }
+
+    for (size_t i = 0; i < m; i++) {
+        std::string str;
+        std::cin >> str;
+        printf("%lld\n", query(str, 0, ROOT_INDEX));
+    }
+
+
+#ifdef _RUN_TIME
+    printf("The running duration is not less than %ld ms\n", clock() - start);
+#endif // _RUN_TIME
+    return 0;
+}
+
+#if READ
+template< typename T >
+inline T readf() {
+#if false
+    T sum = 0;
+    char ch = getchar();
+    while (ch > '9' || ch < '0') ch = getchar();
+    while (ch >= '0' && ch <= '9') sum = sum * 10 + ch - 48, ch = getchar();
+    return sum;
+#else
+    T ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    return sgn ? -ret : ret;
+#endif
+}
+#else
+template< typename Type >
+inline Type readf(Type* p) {
+    Type ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    if (p != NULL) {
+        *p = Type(sgn ? -ret : ret);
+    }
+    return sgn ? -ret : ret;
+}
+#endif
+
+template<typename Type>
+inline void writef(Type x) {
+    int sta[MAX_NUM_SIZE];
+    int top = 0;
+    do {
+        sta[top++] = x % 10, x /= 10;
+    } while (x);
+    while (top) putchar(sta[--top] + '0');  // 48 是 '0'
+    return;
+}
+
+
+
+/**
+ *              ,----------------,              ,---------,
+ *         ,-----------------------,          ,"        ,"|
+ *       ,"                      ,"|        ,"        ,"  |
+ *      +-----------------------+  |      ,"        ,"    |
+ *      |  .-----------------.  |  |     +---------+      |
+ *      |  |                 |  |  |     | -==----'|      |
+ *      |  |  By txp2024     |  |  |     |         |      |
+ *      |  |                 |  |  |     |`---=    |      |
+ *      |  |  C:\>_          |  |  |     |==== ooo |      ;
+ *      |  |                 |  |  |     |(((( [33]|    ,"
+ *      |  `-----------------'  | /      |((((     |  ,"
+ *      +-----------------------+/       |         |,"
+ *         /_)______________(_/          +---------+
+ *    _______________________________
+ *   /  oooooooooooooooo  .o.  oooo /,   /-----------
+ *  / ==ooooooooooooooo==.o.  ooo= //   /\--{)B     ,"
+ * /_==__==========__==_ooo__ooo=_/'   /___________,"
+ *
+ */
