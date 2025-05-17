aabb928e77798609f98079f381bcadbb9b5e789a
新沛谭 <bobi20110114@outlook.com>
2025-05-17 19:39:58 +0800

    12213

--- P12213 [蓝桥杯 2023 国 Python B] 最长回文前后缀.cpp
+++ P12213 [蓝桥杯 2023 国 Python B] 最长回文前后缀.cpp
@@ -18,10 +18,10 @@
 #define READ          false
 #define MAX_INF       1e18
 #define MAX_NUM_SIZE  35
-#define MAX_STR_LENGTH  (uint64_t)(1e5+5)
+#define MAX_LENGTH    (uint64_t)(1e5+5)
 
 typedef long long int ll;
-typedef unsigned long long int ull;
+typedef unsigned long long int unill;
 
 //快读函数声明
 #if READ
@@ -36,99 +36,85 @@ inline Type readf(Type* p = nullptr);
 template<typename Type>
 inline void writef(Type x);
 
-char str1[MAX_STR_LENGTH * 2], str2[MAX_STR_LENGTH * 2];
-ull radius1[MAX_STR_LENGTH * 2], radius2[MAX_STR_LENGTH * 2], first_length1[MAX_STR_LENGTH * 2], first_length2[MAX_STR_LENGTH * 2];
-ull strLength, ans = 0;
+char str[MAX_LENGTH], reverseStr[MAX_LENGTH], manacherStr[MAX_LENGTH * 2 + 3];
+size_t firstLength[MAX_LENGTH], radius[MAX_LENGTH], lastLength[MAX_LENGTH];
+size_t strLength, manacherLength, ans = 0;
 
-inline size_t str_init(char* _Str) {
-    char _s[MAX_STR_LENGTH * 2];
-    _s[0] = '$';
-    ull cnt = 0, _length = strlen(_Str + 1);
-    for (size_t i = 1; i <= _length; i++) {
-        _s[++cnt] = '#';
-        _s[++cnt] = _Str[i];
+inline size_t init() {
+    strLength = strlen(str + 1);
+    size_t cnt = 0;
+    manacherStr[cnt++] = '@';
+    for (size_t i = 1; i <= strLength; i++) {
+        reverseStr[strLength - i + 1] = str[i];
+        manacherStr[cnt++] = '#';
+        manacherStr[cnt++] = str[i];
     }
-    _s[++cnt] = '#';
-    _s[++cnt] = '@';
-    _s[++cnt] = '\0';
-    memcpy(_Str, _s, sizeof(_s));
-    return strlen(_Str);
+    manacherStr[cnt++] = '#';
+    manacherStr[cnt] = '$';
+    return cnt;
 }
 
-inline void _Manacher(const char* str, ull * radius, ull * first_length) {
-    size_t len = strlen(str);
-    std::fill(radius, radius + len, 0);
+inline void manacher() {
     radius[1] = 1;
-    for (ull i = 2, right_pos = 1, left_pos; i < len; i++) {
-        //依据以前计算好的数据拓展
+    for (size_t i = 2, right_pos = 1, left_pos; i < manacherLength; i++) {
+        size_t pre_length = i / 2;
         if (i <= right_pos) {
             radius[i] = std::min(
                 radius[right_pos - i + left_pos],
                 right_pos - i + 1
             );
+           
         }
         //暴力往外拓展
-        while (str[i - radius[i]] == str[i + radius[i]]) {
+        while (manacherStr[i - radius[i]] == manacherStr[i + radius[i]]) {
             ++radius[i];
+        }
+        if (manacherStr[i] == '#') {
+            firstLength[pre_length - (radius[i] - 1) / 2 + 1] = std::max(firstLength[pre_length - (radius[i] - 1) / 2 + 1], radius[i] - 1);
+            lastLength[pre_length + (radius[i] - 1) / 2] = std::max(lastLength[pre_length + (radius[i] - 1) / 2], radius[i] - 1 );
+        }
+        else {
+            firstLength[pre_length - (radius[i] - 1) / 2] = std::max(firstLength[pre_length - (radius[i] - 1) / 2], radius[i] - 1);
+            lastLength[pre_length + (radius[i] - 1) / 2] = std::max(lastLength[pre_length + (radius[i] - 1) / 2], radius[i] - 1);
         }
         if (i + radius[i] - 1 > right_pos) {
             right_pos = i + radius[i] - 1, left_pos = i - radius[i] + 1;
         }
-        first_length[i - radius[i] + 1] = std::max(first_length[i - radius[i] + 1], radius[i] - 1);//求出以该点为左端点的最长饱和回文串 
     }
-    for (int64_t i = 3; i < len; i += 2) {
-        first_length[i] = std::max(first_length[i - 2] - 2, first_length[i]);
-    } 
-}
-
-void Manacher(char* op, ull* r, ull* l) {
-    int mr = 0, c;
-    for (ull i = 1; i < strLength; i++) {
-        if (i < mr) r[i] = std::min(r[2 * c - i], mr - i);
-        else r[i] = 1;
-        while (op[i + r[i]] == op[i - r[i]]) r[i]++;
-        if (i + r[i] - 1 > mr) mr = i + r[i] - 1, c = i;
-        l[i - r[i] + 1] = std::max(l[i - r[i] + 1], r[i] - 1);//求出以该点为左端点的最长饱和回文串 
-    }
-    for (ull i = 3; i < strLength; i += 2) l[i] = std::max(l[i - 2] - 2, l[i]);
-    //求出以该点为左端点的最长回文串（含饱和回文串与不饱和回文串） 
+    return;
 }
 
 int main() {
 #ifdef _FREOPEN
-    freopen("input.txt", "r", stdin);
+    freopen("D:\\bobi2\\development\\work spaces\\OI-code\\input.txt", "r", stdin);
 #endif // _FREOPEN
 
 #ifdef _RUN_TIME
     clock_t start = clock();
 #endif // _RUN_TIME
 
-    scanf("%s\n", str1 + 1);
-    strLength = strlen(str1 + 1);
+    scanf("%s", str + 1);
 
-    for (size_t i = 1; i <= strLength; i++) {
-        str2[i] = str1[strLength - i + 1];
-    } 
+    manacherLength = init();
 
-    strLength = str_init(str1);
-    str_init(str2);
+    manacher();
 
-    _Manacher(str1, radius1, first_length1);
-    _Manacher(str2, radius2, first_length2);
-    
-    for (size_t i = 2; i <= strLength; i += 2) {
-        ans = std::max(std::max(i - 2 + first_length1[i - 1], i - 2 + first_length2[i - 1]), ans);
-        if (str1[i] != str2[i]) {
+    lastLength[0] = 0;
+    firstLength[strLength + 1] = 0;
+    for (size_t i = 0; i <= strLength; i++) {
+        if (str[i] != reverseStr[i] && i != 0) {
             break;
         }
+        ans = std::max(ans, std::max(i * 2 + firstLength[i+1], i * 2 + lastLength[strLength - i]));
     }
 
     printf("%lld\n", ans);
 
-
 #ifdef _RUN_TIME
     printf("The running duration is not less than %ld ms\n", clock() - start);
 #endif // _RUN_TIME
+
+    system("pause");
     return 0;
 }
 
@@ -175,26 +161,3 @@ inline void writef(Type x) {
     while (top) putchar(sta[--top] + '0');  // 48 是 '0'
     return;
 }
-
-
-
-/**
- *              ,----------------,              ,---------,
- *         ,-----------------------,          ,"        ,"|
- *       ,"                      ,"|        ,"        ,"  |
- *      +-----------------------+  |      ,"        ,"    |
- *      |  .-----------------.  |  |     +---------+      |
- *      |  |                 |  |  |     | -==----'|      |
- *      |  |  By txp2024     |  |  |     |         |      |
- *      |  |                 |  |  |     |`---=    |      |
- *      |  |  C:\>_          |  |  |     |==== ooo |      ;
- *      |  |                 |  |  |     |(((( [33]|    ,"
- *      |  `-----------------'  | /      |((((     |  ,"
- *      +-----------------------+/       |         |,"
- *         /_)______________(_/          +---------+
- *    _______________________________
- *   /  oooooooooooooooo  .o.  oooo /,   /-----------
- *  / ==ooooooooooooooo==.o.  ooo= //   /\--{)B     ,"
- * /_==__==========__==_ooo__ooo=_/'   /___________,"
- *
- */
