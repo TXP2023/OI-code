d7acb0ef82c65fed87326d88119f8eada02770f3
user <user@E2>
2025-06-06 19:19:57 +0800

    8818

--- P8818 [CSP-S 2022] 策略游戏.cpp
+++ P8818 [CSP-S 2022] 策略游戏.cpp
@@ -35,6 +35,67 @@ inline Type readf(Type* p = nullptr);
 template<typename Type>
 inline void writef(Type x);
 
+template<size_t __MAX_VALUE>
+class Log2 {
+public:
+    Log2() {
+        log_[1] = 0;
+        log_[1] = 0;
+        log_[2] = 1;
+        for (size_t i = 3; i <= __MAX_VALUE; i++) {
+            log_[i] = log_[i / 2] + 1;
+        }
+        return;
+    }
+
+    int operator ()(const size_t& val) {
+        return log_[val];
+    }
+
+private:
+    int log_[__MAX_VALUE + 1];
+};
+
+template<size_t _size, size_t _size_log>
+struct StTable {
+    ll arrMax[_size + 1][_size_log + 1], arrMin[_size + 1][_size_log + 1];
+    ll arrMax
+    Log2<_size> get_log2;
+
+    inline void insert(size_t pos, ll val) {
+        arrMax[pos][0] = val;
+        arrMin[pos][0] = val;
+        return;
+    }
+
+    inline void init() {
+        for (size_t i = 1; i <= _size_log; i++) {
+            for (size_t j = 1; j + (1 << i) - 1 <= _size; j++) {
+                arrMax[j][i] = std::max(
+                    arrMax[j][i - 1],
+                    arrMax[j + (1 << (i - 1))][i - 1]
+                );
+                arrMin[j][i] = std::min(
+                    arrMin[j][i - 1],
+                    arrMin[j + (1 << (i - 1))][i - 1]
+                );
+            }
+        }
+    }
+
+    inline ll get_max(size_t left, size_t right) {
+        size_t Log = get_log2(right - left + 1);
+        return std::max(arrMax[left][Log], arrMax[right - (1 << Log) + 1][Log]);
+    }
+
+    inline ll get_min(size_t left, size_t right) {
+        size_t Log = get_log2(right - left + 1);
+        return std::min(arrMin[left][Log], arrMin[right - (1 << Log) + 1][Log]);
+    }
+
+};
+
+ll n, m, q;
 
 int main() {
 #ifdef _FREOPEN
@@ -44,6 +105,8 @@ int main() {
 #ifdef _RUN_TIME
     clock_t start = clock();
 #endif // _RUN_TIME
+
+    readf(&n), readf(&m), readf(&q);
 
 
 
