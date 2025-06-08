eaeb022b8f3933ccffe81bcd8e4d00f44ea1286f
user <user@E2>
2025-06-08 10:11:07 +0800

    ac P11379

---
+++ P11379 [GESP202412 八级] 树上移动.cpp
@@ -0,0 +1,177 @@
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+//
+//      By txp2024 www.luogu.com.cn  TXP2023 www.github.com
+// 
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+#pragma once
+#include <vector>
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <ctype.h>
+#include <cstdarg>
+#include <climits>
+#include <time.h>
+#include <iostream>
+#include <stdint.h>
+
+#define READ          false
+#define MAX_INF       1e18
+#define MAX_NUM_SIZE  35
+#define MAXN          105
+#define MAXM          10005
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+//快读函数声明
+#if READ
+template< typename T >
+inline T readf();
+#else
+template< typename Type >
+inline Type readf(Type* p = nullptr);
+#endif
+
+//快速输出函数
+template<typename Type>
+inline void writef(Type x);
+
+struct Edge {
+    size_t u, v;
+    ll w;
+    size_t next;
+};
+
+size_t head[MAXN], inStamp[MAXN], outStamp[MAXN], dfs[MAXN];
+Edge edges[MAXM];
+ll color[MAXN];
+ll n, k, edgeCnt = 0, dfsCnt = 0, ans = 0;
+
+inline void addEdge(ll u, ll v, ll w) {
+    edges[++edgeCnt].u = u;
+    edges[edgeCnt].v = v;
+    edges[edgeCnt].w = w;
+    edges[edgeCnt].next = head[u];
+    head[u] = edgeCnt;
+    return;
+}
+
+void get_dfs(ll u, ll father, ll val, ll num) {
+    ++num;
+    val += color[u];
+    if (val > k) {
+        return;
+    }
+    ans = std::max(ans, num);
+    for (size_t i = head[u]; i != 0; i = edges[i].next) {
+        if (edges[i].v == father) {
+            continue;
+        }
+        get_dfs(edges[i].v, u, val, num);
+    }
+    return;
+}
+
+
+
+int main() {
+#ifdef _FREOPEN
+    freopen("input.txt", "r", stdin);
+#endif // _FREOPEN
+
+#ifdef _RUN_TIME
+    clock_t start = clock();
+#endif // _RUN_TIME
+
+    readf(&n), readf(&k);
+
+    for (size_t i = 1; i <= n; i++) {
+        readf(&color[i]);
+    }
+
+    for (size_t i = 1; i <= n - 1; i++) {
+        ll u = readf<ll>(), v = readf<ll>(), w = 0;
+        addEdge(u, v, w);
+        addEdge(v, u, w);
+    }
+
+
+    for (size_t i = 1; i <= n; i++) {
+        get_dfs(i, 0, 0, 0);
+    }
+
+    printf("%lld\n", ans);
+
+#ifdef _RUN_TIME
+    printf("The running duration is not less than %ld ms\n", clock() - start);
+#endif // _RUN_TIME
+    return 0;
+}
+
+#if READ
+template< typename T >
+inline T readf() {
+#if false
+    T sum = 0;
+    char ch = getchar();
+    while (ch > '9' || ch < '0') ch = getchar();
+    while (ch >= '0' && ch <= '9') sum = sum * 10 + ch - 48, ch = getchar();
+    return sum;
+#else
+    T ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    return sgn ? -ret : ret;
+#endif
+}
+#else
+template< typename Type >
+inline Type readf(Type* p) {
+    Type ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    if (p != NULL) {
+        *p = Type(sgn ? -ret : ret);
+    }
+    return sgn ? -ret : ret;
+}
+#endif
+
+template<typename Type>
+inline void writef(Type x) {
+    int sta[MAX_NUM_SIZE];
+    int top = 0;
+    do {
+        sta[top++] = x % 10, x /= 10;
+    } while (x);
+    while (top) putchar(sta[--top] + '0');  // 48 是 '0'
+    return;
+}
+
+
+
+/**
+ *              ,----------------,              ,---------,
+ *         ,-----------------------,          ,"        ,"|
+ *       ,"                      ,"|        ,"        ,"  |
+ *      +-----------------------+  |      ,"        ,"    |
+ *      |  .-----------------.  |  |     +---------+      |
+ *      |  |                 |  |  |     | -==----'|      |
+ *      |  |  By txp2024     |  |  |     |         |      |
+ *      |  |                 |  |  |     |`---=    |      |
+ *      |  |  C:\>_          |  |  |     |==== ooo |      ;
+ *      |  |                 |  |  |     |(((( [33]|    ,"
+ *      |  `-----------------'  | /      |((((     |  ,"
+ *      +-----------------------+/       |         |,"
+ *         /_)______________(_/          +---------+
+ *    _______________________________
+ *   /  oooooooooooooooo  .o.  oooo /,   /-----------
+ *  / ==ooooooooooooooo==.o.  ooo= //   /\--{)B     ,"
+ * /_==__==========__==_ooo__ooo=_/'   /___________,"
+ *
+ */
