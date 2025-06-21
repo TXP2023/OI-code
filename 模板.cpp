13a27f36fc42cece1cbf9d886b26f29b6eeec523
新沛谭 <bobi20110114@outlook.com>
2025-06-21 19:07:34 +0800

    模板

--- 模板.cpp
+++ 模板.cpp
@@ -16,7 +16,6 @@
 #include <iostream>
 #include <stdint.h>
 
-#define READ          false
 #define MAX_INF       1e18
 #define MAX_NUM_SIZE  35
 
@@ -24,13 +23,8 @@ typedef long long int ll;
 typedef unsigned long long int ull;
 
 //快读函数声明
-#if READ
-template< typename T >
-inline T readf();
-#else
 template< typename Type >
 inline Type readf(Type* p = nullptr);
-#endif
 
 //快速输出函数
 template<typename Type>
@@ -56,25 +50,6 @@ int main() {
     return 0;
 }
 
-#if READ
-template< typename T >
-inline T readf() {
-#if false
-    T sum = 0;
-    char ch = getchar();
-    while (ch > '9' || ch < '0') ch = getchar();
-    while (ch >= '0' && ch <= '9') sum = sum * 10 + ch - 48, ch = getchar();
-    return sum;
-#else
-    T ret = 0, sgn = 0, ch = getchar();
-    while (!isdigit(ch)) {
-        sgn |= ch == '-', ch = getchar();
-    }
-    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
-    return sgn ? -ret : ret;
-#endif
-}
-#else
 template< typename Type >
 inline Type readf(Type* p) {
     Type ret = 0, sgn = 0, ch = getchar();
@@ -82,12 +57,12 @@ inline Type readf(Type* p) {
         sgn |= ch == '-', ch = getchar();
     }
     while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
-    if (p != NULL){
+    if (p != nullptr){
         *p = Type(sgn ? -ret : ret);
     }
     return sgn ? -ret : ret;
 }
-#endif
+
 
 template<typename Type>
 inline void writef(Type x) {
