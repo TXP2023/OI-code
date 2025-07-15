581d457fd7050ab845265a7e499e873a851783eb
txp-computer_ZHAN-99 <bobi20110114@outlook.com>
2025-07-15 17:55:13 +0800

    17073

---
+++ 17073 珠心算测验.cpp
@@ -0,0 +1,121 @@
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+//
+//      By txp2024 www.luogu.com.cn  TXP2023 www.github.com
+// 
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+#pragma once
+#include <vector>
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <numeric>
+#include <ctype.h>
+#include <cstdarg>
+#include <climits>
+#include <time.h>
+#include <iostream>
+#include <stdint.h>
+#include <set>
+
+#define MAX_INF       1e18
+#define MAX_NUM_SIZE  35
+#define MAXN          101
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+//快读函数声明
+template< typename Type >
+inline Type readf(Type* p = nullptr);
+
+//快速输出函数
+template<typename Type>
+inline void writef(Type x);
+
+std::set<ll> set;
+ll arr[MAXN];
+ll n, ans;
+
+int main() {
+#ifdef _FREOPEN
+    freopen("input.txt", "r", stdin);
+#endif // _FREOPEN
+
+#ifdef _RUN_TIME
+    clock_t start = clock();
+#endif // _RUN_TIME
+
+    readf(&n);
+
+    for (size_t i = 1; i <= n; i++) {
+        readf(&arr[i]);
+        set.insert(arr[i]);
+    }
+    
+    std::sort(arr + 1, arr + 1 + n);
+
+    for (size_t i = 2; i <= n; i++) {
+        for (size_t j = 1; j < i; j++) {
+            if (arr[i] - arr[j] != arr[j] && set.count(arr[i] - arr[j])) {
+                ++ans;
+                break;
+            }
+        }
+    }
+
+    printf("%lld\n", ans);
+
+#ifdef _RUN_TIME
+    printf("The running duration is not less than %ld ms\n", clock() - start);
+#endif // _RUN_TIME
+    return 0;
+}
+
+template< typename Type >
+inline Type readf(Type* p) {
+    Type ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    if (p != nullptr) {
+        *p = Type(sgn ? -ret : ret);
+    }
+    return sgn ? -ret : ret;
+}
+
+
+template<typename Type>
+inline void writef(Type x) {
+    int sta[MAX_NUM_SIZE];
+    int top = 0;
+    do {
+        sta[top++] = x % 10, x /= 10;
+    } while (x);
+    while (top) putchar(sta[--top] + '0');  // 48 是 '0'
+    return;
+}
+
+
+
+/**
+ *              ,----------------,              ,---------,
+ *         ,-----------------------,          ,"        ,"|
+ *       ,"                      ,"|        ,"        ,"  |
+ *      +-----------------------+  |      ,"        ,"    |
+ *      |  .-----------------.  |  |     +---------+      |
+ *      |  |                 |  |  |     | -==----'|      |
+ *      |  |                 |  |  |     |         |      |
+ *      |  |  C:\>rp++       |  |  |     |`---=    |      |
+ *      |  |                 |  |  |     |==== ooo |      ;
+ *      |  |                 |  |  |     |(((( [33]|    ,"
+ *      |  `-----------------'  | /      |((((     |  ,"
+ *      +-----------------------+/       |         |,"
+ *         /_)______________(_/          +---------+
+ *    _______________________________
+ *   /  oooooooooooooooo  .o.  oooo /,   /-----------
+ *  / ==ooooooooooooooo==.o.  ooo= //   /\--{)B     ,"
+ * /_==__==========__==_ooo__ooo=_/'   /___________,"
+ *
+ */
