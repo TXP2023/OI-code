f5474f858c2d31aaed29bdb45b456b18b1efa6d7
txp-computer_ZHAN-99 <bobi20110114@outlook.com>
2025-07-16 23:43:38 +0800

    1313

---
+++ 数学.cpp
@@ -0,0 +1,14 @@
+
+template <typename T>
+T gcd(T a, T b) {
+	if (b == 0) {
+		return a;
+	} 
+	return gcd(b, a % b);
+}
+
+template <typename T>
+T lcm(T a, T b) {
+	return a * b / gcd(b, a % b);
+}
+
