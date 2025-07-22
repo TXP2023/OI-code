f026c1067c0dcc7d22f6407662d46764bf3215a2
txp-computer_ZHAN-99 <bobi20110114@outlook.com>
2025-07-22 23:03:58 +0800

    思维题

---
+++ P1419 寻找段落.cpp
@@ -0,0 +1,129 @@
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+//
+//      By txp2024 www.luogu.com.cn  TXP2023 www.github.com
+// 
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+#pragma once
+#include <vector>
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <numeric>
+#include <ctype.h>
+#include <cstdarg>
+#include <climits>
+#include <time.h>
+#include <iostream>
+#include <stdint.h>
+
+#define _FREAD        true
+#define MAX_INF       1e18
+#define MAX_NUM_SIZE  35
+#define MAXN          (size_t)(1e5+5)
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+//快读函数声明
+template< typename Type >
+inline Type readf(Type* p = nullptr);
+
+//快速输出函数
+template<typename Type>
+inline void writef(Type x);
+
+ll preSum[MAXN];
+ll n, s, t;
+double ans = 0;
+
+int main() {
+
+#ifdef _FREOPEN
+    freopen("input.txt", "r", stdin);
+#endif // _FREOPEN
+
+#ifdef _RUN_TIME
+    clock_t start = clock();
+#endif // _RUN_TIME
+
+    readf(&n), readf(&s), readf(&t);
+
+    for (size_t i = 1; i <= n; i++) {
+        preSum[i] = preSum[i - 1] + readf<ll>();
+    }
+
+    for (size_t l = s, r = t; l <= r;) {
+        ll mid = (l + r) >> 1;
+        for (size_t i = 1; i + mid - 1 <= n; i++) {
+            ans = std::max(ans, (double)(preSum[i + mid - 1] - preSum[i - 1]) / mid);
+        }
+    }
+
+    printf("%.3lf\n", ans);
+
+
+#ifdef _RUN_TIME
+    printf("The running duration is not less than %ld ms\n", clock() - start);
+#endif // _RUN_TIME
+    return 0;
+}
+
+template< typename Type >
+inline Type readf(Type* p) {
+#if _FREAD
+    Type ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    if (p != nullptr) {
+        *p = Type(sgn ? -ret : ret);
+    }
+    return sgn ? -ret : ret;
+#else
+    if (p == nullptr) {
+        Type temp;
+        p = &temp;
+    }
+    scanf("%lld", p);
+    return *p;
+
+
+#endif // _FREAD
+}
+
+
+template<typename Type>
+inline void writef(Type x) {
+    int sta[MAX_NUM_SIZE];
+    int top = 0;
+    do {
+        sta[top++] = x % 10, x /= 10;
+    } while (x);
+    while (top) putchar(sta[--top] + '0');  // 48 是 '0'
+    return;
+}
+
+
+
+/**
+ *              ,----------------,              ,---------,
+ *         ,-----------------------,          ,"        ,"|
+ *       ,"                      ,"|        ,"        ,"  |
+ *      +-----------------------+  |      ,"        ,"    |
+ *      |  .-----------------.  |  |     +---------+      |
+ *      |  |                 |  |  |     | -==----'|      |
+ *      |  |                 |  |  |     |         |      |
+ *      |  |  C:\>rp++       |  |  |     |`---=    |      |
+ *      |  |                 |  |  |     |==== ooo |      ;
+ *      |  |                 |  |  |     |(((( [33]|    ,"
+ *      |  `-----------------'  | /      |((((     |  ,"
+ *      +-----------------------+/       |         |,"
+ *         /_)______________(_/          +---------+
+ *    _______________________________
+ *   /  oooooooooooooooo  .o.  oooo /,   /-----------
+ *  / ==ooooooooooooooo==.o.  ooo= //   /\--{)B     ,"
+ * /_==__==========__==_ooo__ooo=_/'   /___________,"
+ *
+ */
