534f21d0099ec6b65bc79392f73a1c14328e5c3e
txp2025 <bobi20110114@outlook.com>
2025-09-24 22:32:59 +0800

    ac P12213

--- P12213 [蓝桥杯 2023 国 Python B] 最长回文前后缀.cpp
+++ P12213 [蓝桥杯 2023 国 Python B] 最长回文前后缀.cpp
@@ -3,161 +3,210 @@
 //      By txp2024 www.luogu.com.cn  TXP2023 www.github.com
 // 
 //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
 #pragma once
-#include <vector>
 #include <stdio.h>
 #include <string.h>
 #include <algorithm>
+#include <numeric>
 #include <ctype.h>
 #include <cstdarg>
+#include <queue>
 #include <climits>
 #include <time.h>
 #include <iostream>
 #include <stdint.h>
+#include <vector>
 
-#define READ          false
-#define MAX_INF       1e18
+#define _FREAD        true
+#define MAX_INF       0x7f7f7f7f7f7f7f7f
 #define MAX_NUM_SIZE  35
-#define MAX_LENGTH    (uint64_t)(1e5+5)
+#define MAX_LEN       (size_t)(1e5+5)
+#define BASE           (131)
+#define MOD            (1 << 31)
 
 typedef long long int ll;
-typedef unsigned long long int unill;
+typedef unsigned long long int ull;
 
-//¿ì¶Áº¯ÊýÉùÃ÷
-#if READ
-template< typename T >
-inline T readf();
-#else
-template< typename Type >
-inline Type readf(Type* p = nullptr);
-#endif
-
-//¿ìËÙÊä³öº¯Êý
-template<typename Type>
-inline void writef(Type x);
-
-char str[MAX_LENGTH], reverseStr[MAX_LENGTH], manacherStr[MAX_LENGTH * 2 + 3];
-size_t firstLength[MAX_LENGTH], radius[MAX_LENGTH], lastLength[MAX_LENGTH];
-size_t strLength, manacherLength, ans = 0;
-
-inline size_t init() {
-    strLength = strlen(str + 1);
-    size_t cnt = 0;
-    manacherStr[cnt++] = '@';
-    for (size_t i = 1; i <= strLength; i++) {
-        reverseStr[strLength - i + 1] = str[i];
-        manacherStr[cnt++] = '#';
-        manacherStr[cnt++] = str[i];
+template<typename _T>
+inline _T fpow(_T a, _T n, _T mod) {
+    _T base = a, ret = 1;
+    while (n) {
+        if (n & 1) {
+            ret = ret * base;
+            ret %= mod;
+        }
+        base = base * base;
+        base %= mod;
+        n >>= 1;
     }
-    manacherStr[cnt++] = '#';
-    manacherStr[cnt] = '$';
-    return cnt;
+    return ret % mod;
 }
 
-inline void manacher() {
-    radius[1] = 1;
-    for (size_t i = 2, right_pos = 1, left_pos; i < manacherLength; i++) {
-        size_t pre_length = i / 2;
-        if (i <= right_pos) {
-            radius[i] = std::min(
-                radius[right_pos - i + left_pos],
-                right_pos - i + 1
-            );
-           
+template<typename _T>
+inline _T fpow(_T a, _T n) {
+    _T base = a, ret = 1;
+    while (n) {
+        if (n & 1) {
+            ret = ret * base;
+            //ret %= mod;
         }
-        //±©Á¦ÍùÍâÍØÕ¹
-        while (manacherStr[i - radius[i]] == manacherStr[i + radius[i]]) {
-            ++radius[i];
-        }
-        if (manacherStr[i] == '#') {
-            firstLength[pre_length - (radius[i] - 1) / 2 + 1] = std::max(firstLength[pre_length - (radius[i] - 1) / 2 + 1], radius[i] - 1);
-            lastLength[pre_length + (radius[i] - 1) / 2] = std::max(lastLength[pre_length + (radius[i] - 1) / 2], radius[i] - 1 );
-        }
-        else {
-            firstLength[pre_length - (radius[i] - 1) / 2] = std::max(firstLength[pre_length - (radius[i] - 1) / 2], radius[i] - 1);
-            lastLength[pre_length + (radius[i] - 1) / 2] = std::max(lastLength[pre_length + (radius[i] - 1) / 2], radius[i] - 1);
-        }
-        if (i + radius[i] - 1 > right_pos) {
-            right_pos = i + radius[i] - 1, left_pos = i - radius[i] + 1;
-        }
+        base = base * base;
+        //base %= mod;
+        n >>= 1;
     }
-    return;
+    return ret;
+}
+//å¿«è¯»å‡½æ•°å£°æ˜Ž
+template< typename Type >
+inline Type fread(Type* p = nullptr);
+
+//å¿«é€Ÿè¾“å‡ºå‡½æ•°
+template<typename Type>
+inline void fwrite(Type x);
+
+ull hash[MAX_LEN], sufHash[MAX_LEN], len;
+char s[MAX_LEN];
+
+inline ll get_hash(ll l, ll r) {
+    if (l > r) {
+        return 0;
+    }
+    return  (hash[r] - hash[l - 1] * fpow(ll BASE, r - l + 1) % MOD + MOD) % MOD;
+}
+
+inline ll get_suf_hash(ll l, ll r) {
+    if (l > r) {
+        return 0;
+    }
+    return  (sufHash[l] - sufHash[r + 1] * fpow(ll BASE, r - l + 1) % MOD + MOD) % MOD;
 }
 
 int main() {
+
 #ifdef _FREOPEN
-    freopen("D:\\bobi2\\development\\work spaces\\OI-code\\input.txt", "r", stdin);
+    freopen("input.txt", "r", stdin);
 #endif // _FREOPEN
 
 #ifdef _RUN_TIME
     clock_t start = clock();
 #endif // _RUN_TIME
 
-    scanf("%s", str + 1);
+    scanf("%s", s + 1);
+    len = strlen(s + 1);
 
-    manacherLength = init();
-
-    manacher();
-
-    lastLength[0] = 0;
-    firstLength[strLength + 1] = 0;
-    for (size_t i = 0; i <= strLength; i++) {
-        if (str[i] != reverseStr[i] && i != 0) {
-            break;
-        }
-        ans = std::max(ans, std::max(i * 2 + firstLength[i+1], i * 2 + lastLength[strLength - i]));
+    for (size_t i = 1; i <= len; i++) {
+        hash[i] = (hash[i - 1] * BASE + s[i]) % MOD;
     }
 
-    printf("%lld\n", ans);
+    for (size_t i = len; i >= 1; --i) {
+        sufHash[i] = (sufHash[i + 1] * BASE + s[i]) % MOD;
+    }
+
+    ll maxLen = 0;
+    for (size_t i = 1; i <= len; i++) {
+        if (get_hash(1, i) == get_suf_hash(len - i + 1, len)) {
+            maxLen = i;
+        }
+    }
+
+    ll lsize = 0;
+    for (size_t i = maxLen + 1; i <= len; i++) {
+        ll mid = ((i - maxLen) >> 1) + maxLen;
+        if ((i - maxLen) & 1) {
+            if (get_hash(maxLen + 1, mid) == get_suf_hash(mid + 2, i)) {
+                lsize = i - maxLen;
+            }
+        }
+        else {
+            if (get_hash(maxLen + 1, mid) == get_suf_hash(mid + 1, i)) {
+                lsize = i - maxLen;
+            }
+        }
+    }
+
+    ll rsize = 0;
+    for (size_t i = len - maxLen; i >= 1; --i) {
+        ll mid = len - maxLen - ((len - maxLen - i + 1) >> 1);
+        if ((len - maxLen - i + 1) & 1) {
+            if (get_hash(i, mid - 1) == get_suf_hash(mid + 1, len - maxLen)) {
+                rsize = len - maxLen - i + 1;
+            }
+        }
+        else {
+            if (get_hash(i, mid) == get_suf_hash(mid + 1, len - maxLen)) {
+                rsize = i - maxLen;
+            }
+        }
+    }
+
+    printf("%lld\n", maxLen * 2 + std::max(lsize, rsize));
+
+
 
 #ifdef _RUN_TIME
     printf("The running duration is not less than %ld ms\n", clock() - start);
 #endif // _RUN_TIME
-
-    system("pause");
     return 0;
 }
 
-#if READ
-template< typename T >
-inline T readf() {
-#if false
-    T sum = 0;
-    char ch = getchar();
-    while (ch > '9' || ch < '0') ch = getchar();
-    while (ch >= '0' && ch <= '9') sum = sum * 10 + ch - 48, ch = getchar();
-    return sum;
-#else
-    T ret = 0, sgn = 0, ch = getchar();
-    while (!isdigit(ch)) {
-        sgn |= ch == '-', ch = getchar();
-    }
-    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
-    return sgn ? -ret : ret;
-#endif
-}
-#else
 template< typename Type >
-inline Type readf(Type* p) {
+inline Type fread(Type* p) {
+#if _FREAD
     Type ret = 0, sgn = 0, ch = getchar();
     while (!isdigit(ch)) {
         sgn |= ch == '-', ch = getchar();
     }
     while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
-    if (p != NULL) {
+    if (p != nullptr) {
         *p = Type(sgn ? -ret : ret);
     }
     return sgn ? -ret : ret;
+#else
+    if (p == nullptr) {
+        Type temp;
+        p = &temp;
+    }
+    scanf("%lld", p);
+    return *p;
+
+
+#endif // _FREAD
 }
-#endif
+
 
 template<typename Type>
-inline void writef(Type x) {
+inline void fwrite(Type x) {
     int sta[MAX_NUM_SIZE];
     int top = 0;
     do {
         sta[top++] = x % 10, x /= 10;
     } while (x);
-    while (top) putchar(sta[--top] + '0');  // 48 ÊÇ '0'
+    while (top) {
+        putchar(sta[--top] + '0');
+    }  // 48 æ˜¯ '0'
     return;
 }
+
+
+
+/**
+ *              ,----------------,              ,---------,
+ *         ,-----------------------,          ,"        ,"|
+ *       ,"                      ,"|        ,"        ,"  |
+ *      +-----------------------+  |      ,"        ,"    |
+ *      |  .-----------------.  |  |     +---------+      |
+ *      |  |                 |  |  |     | -==----'|      |
+ *      |  |                 |  |  |     |         |      |
+ *      |  |  C:\>rp++       |  |  |     |`---=    |      |
+ *      |  |                 |  |  |     |==== ooo |      ;
+ *      |  |                 |  |  |     |(((( [33]|    ,"
+ *      |  `-----------------'  | /      |((((     |  ,"
+ *      +-----------------------+/       |         |,"
+ *         /_)______________(_/          +---------+
+ *    _______________________________
+ *   /  oooooooooooooooo  .o.  oooo /,   /-----------
+ *  / ==ooooooooooooooo==.o.  ooo= //   /\--{)B     ,"
+ * /_==__==========__==_ooo__ooo=_/'   /___________,"
+ *
+ */
