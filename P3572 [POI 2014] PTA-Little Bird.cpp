f50e26994f1e15a573cf3eed9ceb8c92c0107fb8
txp2025 <bobi20110114@outlook.com>
2025-10-05 23:43:06 +0800

    ac P3572 P4552

---
+++ P3572 [POI 2014] PTA-Little Bird.cpp
@@ -0,0 +1,163 @@
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+//
+//      By txp2024 www.luogu.com.cn  TXP2023 www.github.com
+//
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <numeric>
+#include <ctype.h>
+#include <cstdarg>
+#include <queue>
+#include <climits>
+#include <time.h>
+#include <iostream>
+#include <stdint.h>
+#include <vector>
+
+#define _FREAD        true
+#define MAX_INF       0x7f7f7f7f7f7f7f7f
+#define MAX_NUM_SIZE  35
+#define MAXN          (size_t)(1e6+5)
+#define MAXQ          (30)
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+template<typename _T>
+inline _T fpow(_T a, _T n, _T mod) {
+    _T base = a, ret = 1;
+    while (n) {
+        if (n & 1) {
+            ret = ret * base;
+            ret %= mod;
+        }
+        base = base * base;
+        base %= mod;
+        n >>= 1;
+    }
+    return ret % mod;
+}
+//快读函数声明
+template< typename Type >
+inline Type fread(Type* p = nullptr);
+
+//快速输出函数
+template<typename Type>
+inline void fwrite(Type x);
+
+ll h[MAXN], dis[MAXN], dp[MAXN];
+ll n, q;
+
+void slove(ll x) {
+    memset(dp, 0x7f, sizeof dp);
+    dp[1] = 0;
+    std::deque<std::pair<ll, ll>> que; //第一位存权值 第二位存位置  单调队列递增
+    que.push_back(std::make_pair(0, 1));
+    for (size_t i = 2; i <= n; i++) {
+        while (i - que.front().second > dis[x]) {
+            que.pop_front();
+        }
+        dp[i] = dp[que.front().second] + (h[i] >= h[que.front().second]);
+        while (que.size() && (dp[i] < que.back().first || (dp[i] == que.back().first && h[i] >= h[que.back().second]))) {
+            que.pop_back();
+        }
+        que.push_back(std::make_pair(dp[i], i));
+    }
+    printf("%lld\n", dp[n]);
+}
+
+int main() {
+
+#ifdef _FREOPEN
+    freopen("input.txt", "r", stdin);
+#endif // _FREOPEN
+
+#ifdef _RUN_TIME
+    clock_t start = clock();
+#endif // _RUN_TIME
+
+    //freopen("input.in", "r", stdin);
+
+    fread(&n);
+
+    for (size_t i = 1; i <= n; i++) {
+        fread(&h[i]);
+    }
+
+    fread(&q);
+
+    for (size_t i = 1; i <= q; i++) {
+        fread(&dis[i]);
+        slove(i);
+    }
+
+#ifdef _RUN_TIME
+    printf("The running duration is not less than %ld ms\n", clock() - start);
+#endif // _RUN_TIME
+    return 0;
+}
+
+template< typename Type >
+inline Type fread(Type* p) {
+#if _FREAD
+    Type ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    if (p != nullptr) {
+        *p = Type(sgn ? -ret : ret);
+    }
+    return sgn ? -ret : ret;
+#else
+    if (p == nullptr) {
+        Type temp;
+        p = &temp;
+    }
+    scanf("%lld", p);
+    return *p;
+
+
+#endif // _FREAD
+}
+
+
+template<typename Type>
+inline void fwrite(Type x) {
+    int sta[MAX_NUM_SIZE];
+    int top = 0;
+    do {
+        sta[top++] = x % 10, x /= 10;
+    } while (x);
+    while (top) {
+        putchar(sta[--top] + '0');
+    }  // 48 是 '0'
+    return;
+}
+
+
+
+/**
+ *              ,----------------,              ,---------,
+ *         ,-----------------------,          ,"        ,"|
+ *       ,"                      ,"|        ,"        ,"  |
+ *      +-----------------------+  |      ,"        ,"    |
+ *      |  .-----------------.  |  |     +---------+      |
+ *      |  |                 |  |  |     | -==----'|      |
+ *      |  |                 |  |  |     |         |      |
+ *      |  |  C:\>rp++       |  |  |     |`---=    |      |
+ *      |  |                 |  |  |     |==== ooo |      ;
+ *      |  |                 |  |  |     |(((( [33]|    ,"
+ *      |  `-----------------'  | /      |((((     |  ,"
+ *      +-----------------------+/       |         |,"
+ *         /_)______________(_/          +---------+
+ *    _______________________________
+ *   /  oooooooooooooooo  .o.  oooo /,   /-----------
+ *  / ==ooooooooooooooo==.o.  ooo= //   /\--{)B     ,"
+ * /_==__==========__==_ooo__ooo=_/'   /___________,"
+ *
+ */
