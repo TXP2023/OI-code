5f7ee3003377dd0c91bcada5e5cc4d8ec2713aa9
txp2025 <bobi20110114@outlook.com>
2025-10-06 15:15:31 +0800

    4053

---
+++ P4053 [JSOI2007] 建筑抢修.cpp
@@ -0,0 +1,170 @@
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+//
+//      By txp2024 www.luogu.com.cn  TXP2023 www.github.com
+// 
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <numeric>
+#include <ctype.h>
+#include <cstdarg>
+#include <queue>
+#include <climits>
+#include <time.h>
+#include <iostream>
+#include <stdint.h>
+#include <vector>
+
+#define _FREAD        true
+#define MAX_INF       0x7f7f7f7f7f7f7f7f
+#define MAX_NUM_SIZE  35
+#define MAXN          150005
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+template<typename _T>
+inline _T fpow(_T a, _T n, _T mod) {
+    _T base = a, ret = 1;
+    while (n) {
+        if (n & 1) {
+            ret = ret * base;
+            ret %= mod;
+        }
+        base = base * base;
+        base %= mod;
+        n >>= 1;
+    }
+    return ret % mod;
+}
+//快读函数声明
+template< typename Type >
+inline Type fread(Type* p = nullptr);
+
+//快速输出函数
+template<typename Type>
+inline void fwrite(Type x);
+
+struct Build {
+    ll cost, end;
+
+    bool operator <(const Build& other)const {
+        return cost < other.cost;
+    }
+};
+
+Build b[MAXN];
+ll n, ans = 0;
+
+int main() {
+
+#ifdef _FREOPEN
+    freopen("input.txt", "r", stdin);
+#endif // _FREOPEN
+
+#ifdef _RUN_TIME
+    clock_t start = clock();
+#endif // _RUN_TIME
+
+    fread(&n);
+
+    for (size_t i = 1; i <= n; i++) {
+        fread(&b[i].cost), fread(&b[i].end);
+    }
+
+
+    std::sort(b + 1, b + 1 + n, [](Build a, Build b) {
+        if (a.end == b.end) {
+            return a.cost < b.cost;
+        }
+        return a.end < b.end;
+    });
+
+    std::priority_queue<ll> heap;
+
+    ll t = 0;
+    for (size_t i = 1; i <= n; i++) {
+        if (t + b[i].cost <= b[i].end) {
+            t += b[i].cost;
+            heap.push(b[i].cost);
+            ++ans;
+            continue;
+        }
+        if (!heap.empty() && heap.top() > b[i].cost) {
+            t -= heap.top() - b[i].cost;
+            heap.pop();
+            heap.push(b[i].cost);
+        }
+    }
+
+    printf("%lld\n", ans);
+
+#ifdef _RUN_TIME
+    printf("The running duration is not less than %ld ms\n", clock() - start);
+#endif // _RUN_TIME
+    return 0;
+}
+
+template< typename Type >
+inline Type fread(Type* p) {
+#if _FREAD
+    Type ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    if (p != nullptr) {
+        *p = Type(sgn ? -ret : ret);
+    }
+    return sgn ? -ret : ret;
+#else
+    if (p == nullptr) {
+        Type temp;
+        p = &temp;
+    }
+    scanf("%lld", p);
+    return *p;
+
+
+#endif // _FREAD
+}
+
+
+template<typename Type>
+inline void fwrite(Type x) {
+    int sta[MAX_NUM_SIZE];
+    int top = 0;
+    do {
+        sta[top++] = x % 10, x /= 10;
+    } while (x);
+    while (top) {
+        putchar(sta[--top] + '0');
+    }  // 48 是 '0'
+    return;
+}
+
+
+
+/**
+ *              ,----------------,              ,---------,
+ *         ,-----------------------,          ,"        ,"|
+ *       ,"                      ,"|        ,"        ,"  |
+ *      +-----------------------+  |      ,"        ,"    |
+ *      |  .-----------------.  |  |     +---------+      |
+ *      |  |                 |  |  |     | -==----'|      |
+ *      |  |                 |  |  |     |         |      |
+ *      |  |  C:\>rp++       |  |  |     |`---=    |      |
+ *      |  |                 |  |  |     |==== ooo |      ;
+ *      |  |                 |  |  |     |(((( [33]|    ,"
+ *      |  `-----------------'  | /      |((((     |  ,"
+ *      +-----------------------+/       |         |,"
+ *         /_)______________(_/          +---------+
+ *    _______________________________
+ *   /  oooooooooooooooo  .o.  oooo /,   /-----------
+ *  / ==ooooooooooooooo==.o.  ooo= //   /\--{)B     ,"
+ * /_==__==========__==_ooo__ooo=_/'   /___________,"
+ *
+ */
