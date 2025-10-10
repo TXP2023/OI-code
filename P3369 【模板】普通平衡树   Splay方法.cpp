d3203e0ed216338a8ee3d92c3da36fbafe3b0f8f
txp2025 <bobi20110114@outlook.com>
2025-10-10 19:19:47 +0800

    Splayj

---
+++ P3369 【模板】普通平衡树   Splay方法.cpp
@@ -0,0 +1,169 @@
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+//
+//      By txp2024 www.luogu.com.cn  TXP2023 www.github.com
+// 
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <numeric>
+#include <ctype.h>
+#include <cstdarg>
+#include <queue>
+#include <climits>
+#include <time.h>
+#include <iostream>
+#include <stdint.h>
+#include <vector>
+
+#define _FREAD        true
+#define MAX_INF       0x7f7f7f7f7f7f7f7f
+#define MAX_NUM_SIZE  35
+#define MAXN          (size_t)(1e5+5)
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+template<typename _T>
+inline _T fpow(_T a, _T n, _T mod) {
+    _T base = a, ret = 1;
+    while (n) {
+        if (n & 1) {
+            ret = ret * base;
+            ret %= mod;
+        }
+        base = base * base;
+        base %= mod;
+        n >>= 1;
+    }
+    return ret % mod;
+}
+//快读函数声明
+template< typename Type >
+inline Type fread(Type* p = nullptr);
+
+//快速输出函数
+template<typename Type>
+inline void fwrite(Type x);
+
+struct node {
+    ll val, cnt, size;
+    ll fa, son[2];
+
+    node() = default;
+};
+
+node tree[MAXN];
+ll n;
+
+ll pre(ll x);
+
+ll next(ll x);
+
+//update
+void upd(ll x) {
+
+}
+
+//旋转x
+void rotate(ll x) {
+
+}
+
+//将u旋转为v的儿子， 当v=0时，将u设为root
+void Splay(ll u, ll v) {
+    ll fa = tree[u].fa, gfa = tree[fa].fa; //fa grandfa
+
+
+}
+
+//insert
+void ins(ll x);
+
+//remove
+void rm(ll x);
+
+int main() {
+
+#ifdef _FREOPEN
+    freopen("input.txt", "r", stdin);
+#endif // _FREOPEN
+
+#ifdef _RUN_TIME
+    clock_t start = clock();
+#endif // _RUN_TIME
+
+    fread(&n);
+
+
+
+
+
+#ifdef _RUN_TIME
+    printf("The running duration is not less than %ld ms\n", clock() - start);
+#endif // _RUN_TIME
+    return 0;
+}
+
+template< typename Type >
+inline Type fread(Type* p) {
+#if _FREAD
+    Type ret = 0, sgn = 0, ch = getchar();
+    while (!isdigit(ch)) {
+        sgn |= ch == '-', ch = getchar();
+    }
+    while (isdigit(ch)) ret = ret * 10 + ch - '0', ch = getchar();
+    if (p != nullptr) {
+        *p = Type(sgn ? -ret : ret);
+    }
+    return sgn ? -ret : ret;
+#else
+    if (p == nullptr) {
+        Type temp;
+        p = &temp;
+    }
+    scanf("%lld", p);
+    return *p;
+
+
+#endif // _FREAD
+}
+
+
+template<typename Type>
+inline void fwrite(Type x) {
+    int sta[MAX_NUM_SIZE];
+    int top = 0;
+    do {
+        sta[top++] = x % 10, x /= 10;
+    } while (x);
+    while (top) {
+        putchar(sta[--top] + '0');
+    }  // 48 是 '0'
+    return;
+}
+
+
+
+/**
+ *              ,----------------,              ,---------,
+ *         ,-----------------------,          ,"        ,"|
+ *       ,"                      ,"|        ,"        ,"  |
+ *      +-----------------------+  |      ,"        ,"    |
+ *      |  .-----------------.  |  |     +---------+      |
+ *      |  |                 |  |  |     | -==----'|      |
+ *      |  |                 |  |  |     |         |      |
+ *      |  |  C:\>rp++       |  |  |     |`---=    |      |
+ *      |  |                 |  |  |     |==== ooo |      ;
+ *      |  |                 |  |  |     |(((( [33]|    ,"
+ *      |  `-----------------'  | /      |((((     |  ,"
+ *      +-----------------------+/       |         |,"
+ *         /_)______________(_/          +---------+
+ *    _______________________________
+ *   /  oooooooooooooooo  .o.  oooo /,   /-----------
+ *  / ==ooooooooooooooo==.o.  ooo= //   /\--{)B     ,"
+ * /_==__==========__==_ooo__ooo=_/'   /___________,"
+ *
+ */
